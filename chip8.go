6fb12337726d3eaa42e05f1c45c79c3a3bef1116
André Taiar <taiar@hotmail.com>
2026-03-17 18:34:23 -0300

    More opcodes from our tests

--- chip8.go
+++ chip8.go
@@ -58,10 +58,14 @@ func (c *Chip8) Cycle() {
 }
 
 func (c *Chip8) Execute(opcode uint16) {
-	// Extrai o primeiro dígito hexadecimal (ex: de 0xA2F0, pega o 'A')
-	firstNibble := opcode & 0xF000
+	// Extraímos os componentes comuns para facilitar a leitura
+	x := (opcode & 0x0F00) >> 8 // O segundo nibble
+	y := (opcode & 0x00F0) >> 4 // O terceiro nibble
+	n := opcode & 0x000F        // O último nibble
+	// nnn := opcode & 0x0FFF     // Os últimos 12 bits (endereço)
+	nn := byte(opcode & 0x00FF) // Os últimos 8 bits (valor imediato)
 
-	switch firstNibble {
+	switch opcode & 0xF000 {
 	case 0x0000:
 		// Limpa o array do display colocando 0 em tudo
 		for i := range c.Display {
@@ -82,6 +86,45 @@ func (c *Chip8) Execute(opcode uint16) {
 		address := opcode & 0x0FFF
 		c.I = address
 		fmt.Printf("Instrução: I = 0x%X\n", address)
+
+	case 0x6000:
+		// 0x6XNN (Set Register VX)
+		c.V[x] = nn
+
+	case 0xF000:
+		// 0xFX29 (Set I to Sprite Location)
+		switch nn {
+		case 0x29:
+			// c.V[x] contém o caractere (ex: 0x0B).
+			// Multiplicamos por 5 porque cada sprite tem 5 bytes de altura.
+			c.I = uint16(0x50) + (uint16(c.V[x]) * 5)
+		}
+
+	case 0xD000:
+		// 0xDXYN (Draw Sprite)
+		xCoord := c.V[x] % 64 // Wrap around (opcional, depende da ROM)
+		yCoord := c.V[y] % 32
+		c.V[0xF] = 0 // Reseta o flag de colisão
+
+		for row := uint16(0); row < n; row++ {
+			spriteByte := c.Memory[c.I+row]
+			for col := uint16(0); col < 8; col++ {
+				// Verifica se o bit específico do spriteByte está ligado (1)
+				// Começamos do bit mais significativo (0x80)
+				if (spriteByte & (0x80 >> col)) != 0 {
+					// Cálculo do índice no array linear da tela
+					idx := (uint16(xCoord) + col) + ((uint16(yCoord) + row) * 64)
+
+					// Se o índice estourar a tela, paramos ou fazemos wrap
+					if idx < 64*32 {
+						if c.Display[idx] == 1 {
+							c.V[0xF] = 1 // Colisão detectada!
+						}
+						c.Display[idx] ^= 1 // XOR: Liga se estava desligado, desliga se estava ligado
+					}
+				}
+			}
+		}
 
 	default:
 		fmt.Printf("Opcode não implementado: 0x%X\n", opcode)
